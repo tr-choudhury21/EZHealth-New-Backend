4dbac708467cf7046214a890ffcfd2ed92841737
Titu Ray Choudhury <tituraychoudhury@gmail.com>
2025-07-04 22:22:59 +0530

    add new routes

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import { dbConnect } from "./database/db.js";
-import userRoutes from "./routes/userRoutes.js";
-import docRoutes from "./routes/docRoutes.js";
-import appoinmentRoutes from "./routes/appointmentRoutes.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { dbConnect } from './database/db.js';
+import userRoutes from './routes/userRoutes.js';
+import docRoutes from './routes/docRoutes.js';
+import adminRoutes from './routes/adminRoutes.js';
+import appoinmentRoutes from './routes/appointmentRoutes.js';
+import paymentRoutes from './routes/paymentRoutes.js';
+import aiRoutes from './routes/aiRoutes.js';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpec } from './utils/swaggerConfig.js';
 
 dotenv.config();
 
@@ -14,21 +19,25 @@ const app = express();
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
 );
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //connecting to the database
 dbConnect();
 
 //importing routes
-app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/doctor", docRoutes);
-app.use("/api/v1/appointment", appoinmentRoutes);
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/doctor', docRoutes);
+app.use('/api/v1/admin', adminRoutes);
+app.use('/api/v1/appointment', appoinmentRoutes);
+app.use('/api/v1/payment', paymentRoutes);
+app.use('/api/v1/ai', aiRoutes);
 
 //start the server
 app.listen(process.env.PORT, () => {
