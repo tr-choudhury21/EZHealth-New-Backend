6199584a2358e9447b11e0b538baddc443800305
Titu Ray Choudhury <tituraychoudhury@gmail.com>
2026-05-22 00:40:30 +0530

    change test url

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export const dbConnect = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
-      dbName: "EZHealth",
+    .connect(process.env.MONGO_URI_TEST, {
+      dbName: 'EZHealth',
     })
     .then(() => {
-      console.log("MongoDB connected successfully.");
+      console.log('MongoDB connected successfully.');
     })
     .catch((err) => {
       console.log(`Some error occurred while connecting to database: ${err}`);
